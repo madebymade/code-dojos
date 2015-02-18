96255d0415a9f8f30aea80f806656f733db05cce
Scott Mason <scott@madebymade.co.uk>
2015-02-18 10:34:22 +0000

    1st test passing

---
+++ CoinSums/coin_sums.rb
@@ -0,0 +1,9 @@
+class CoinSum
+  def initialize(config)
+    @sum = config['sum']
+  end
+
+  def calc
+    1
+  end
+end
--- CoinSums/spec.rb
+++ CoinSums/spec.rb
@@ -1,1 +1,9 @@
 require_relative './coin_sums'
+
+describe CoinSum do
+  subject { described_class.new(sum: sum).calc }
+  context 'when we set a sum of 1p' do
+    let(:sum) { 1 }
+    it { is_expected.to eq 1 }
+  end
+end
