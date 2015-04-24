4441ba68026ca84297a4c0a790fabd9bb4072b94
Scott Mason <scott@madebymade.co.uk>
2015-04-24 11:07:40 +0100

    Allow player increment score

--- GameGrid/js/player.js
+++ GameGrid/js/player.js
@@ -2,4 +2,8 @@ window.Player = function() {
   this.score = 0;
   this.x = 0;
   this.y = 0;
+
+  this.pickUp = function(collectable) {
+    this.score = this.score + collectable.points
+  }
 }
--- GameGrid/specs/WorldSpec.js
+++ GameGrid/specs/WorldSpec.js
@@ -1,36 +1,44 @@
-describe("Player", function() {
+describe('Player', function() {
   var player;
 
   beforeEach(function() {
     world = createWorld(25, 25),
-    player = createPlayer();
+    player = new Player();
   });
 
-  describe("ContstrainBoundaries", function() {
-    it("allows player to move to valid tile", function() {
+  describe('ContstrainBoundaries', function() {
+    it('allows player to move to valid tile', function() {
       expect(canMove(world, player, 1, 0)).toEqual(true);
     });
 
-    it("prevents player from moving to an invalid position", function() {
+    it('prevents player from moving to an invalid position', function() {
       expect(canMove(world, player, -1, 0)).toEqual(false);
       expect(canMove(world, player, 26, 0)).toEqual(false);
     });
   });
 
-  describe("RandomPositionsInWorld", function() {
-    it("generates 10 random positions", function() {
+  describe('RandomPositionsInWorld', function() {
+    it('generates 10 random positions', function() {
       expect(RandomPositionsInWorld(10).length).toEqual(10)
     });
 
-    it("returns positions within the x and y coordinates", function() {
+    it('returns positions within the x and y coordinates', function() {
       result = RandomPositionsInWorld(10)
       first_result = result[0];
       expect(first_result.x).toBeLessThan(world.length);
     });
-
-    it("collects a collectable item", function() {
-      expect(player.score).toEqual(1);
-    });
   });
 
+  describe('when collecting burgers', function() {
+    beforeEach(function() {
+      burger = {
+        points: 10
+      }
+    });
+
+    it('should increase the players score', function() {
+      player.pickUp(burger);
+      expect(player.score).toEqual(10);
+    });
+  });
 });
